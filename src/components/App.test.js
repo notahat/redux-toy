efd1ecaf90303f7de210e2c8f0b535e8be42f3c8
Pete Yandell <pete@notahat.com>
2019-09-13 09:42:07 +1000

    Unbreak the App test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+import reducer from "../state/reducer"
 
 it("renders without crashing", () => {
   const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
+  const store = createStore(reducer)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
